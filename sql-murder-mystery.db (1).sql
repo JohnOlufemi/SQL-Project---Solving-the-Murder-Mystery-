a9fb64e836c2ddb82a7002399aa810af8f769f3d
John Ogunleye <104203741+JohnOlufemi@users.noreply.github.com>
2023-06-19 20:39:47 +0100

    Update sql-murder-mystery.db (1).sql

--- sql-murder-mystery.db (1).sql
+++ sql-murder-mystery.db (1).sql
@@ -1,16 +1,11 @@
-
 --OVERVIEW:
-
 --A crime has taken place and the detective needs your help. 
 --The detective gave you the crime scene report, but you somehow lost it. 
 --You vaguely remember that the crime was a murder that occurred sometime on Jan.15, 2018 and that it took place in SQL City. 
 --Start by retrieving the corresponding crime scene report from the police department’s database. 
 --Figure out who committed the crime with the details you remembered above.
 
-
-
-
---Step 1:Retrived the crime scene report from the police department database
+--Step 1: Retrieve the crime scene report from the police department database
 Select *
 from crime_scene_report
 
